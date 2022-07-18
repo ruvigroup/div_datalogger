f18225d04c0ec5ea73bba55a18120e23b25ba146
Tim Schimansky <tim-schimansky@live.de>
2022-07-18 18:26:50 +0200

    Fitted remote logging possibility to needs of sensor logging bike. Removal of button, STLs and launch files. Made topics dependend on launch file.

--- scripts/div_datalogger_node.py
+++ scripts/div_datalogger_node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Node subscribing to sensor topics and handling user request via service
 #
@@ -8,19 +8,12 @@
 #  SHUTDOWN
 #
 # Subscribed topics:
-#  /scan 							: laser scan topic
-#  /imu/data 						: laser scan topic
-#  /time_reference					: GPS ref time
-#  /vel								: GPS speed
-#  /fix 							: GPS position
-#  /flagbutton_pressed 				: flag button is pressed topic
-#  /cv_camera/image_raw/compressed 	: image raw data (compressed)
-#  /rosout							: ros log output
+#  come as parameter from launch file
 #
 # Service:
 #  /user_cmd
 # 	-> Request:		ID
-#	-> Response:	State ID, Message ID, Recording time 
+#	-> Response:	State ID, Message ID, Recording time
 
 import roslib
 import rospy
@@ -36,9 +29,10 @@ from div_datalogger.srv import *
 
 
 class Datalogger:
-	def __init__(self):
-		rospy.init_node("div_datalogger")
-		
+	def __init__(self, topic_list, internal_path, external_path):
+		# Create name for node
+		rospy.init_node("remote_bag_logging")
+
 		# State ID's
 		self.__STATE_ID_IDLE		= 0
 		self.__STATE_ID_REC			= 1
@@ -63,7 +57,12 @@ class Datalogger:
 		self.__state_id			= self.__STATE_ID_IDLE
 		self.__proc_rec 		= []
 
-	
+		# List of topics to record
+		self.topic_list = topic_list
+
+		# Paths from launch file for bag file destination
+		self.internal_path = internal_path
+		self.external_path = external_path
 
 	def __handle_user_cmd(self, request):
 		if request.request_id == self.__REQ_ID_STATUS:
@@ -76,23 +75,25 @@ class Datalogger:
 			if self.__state_id == self.__STATE_ID_IDLE:
 				# Prepare log message
 				rospy.loginfo(rospy.get_caller_id() + ': Start recording bag file...')
-				# Find USB storage device as first directory in /media/ubuntu/
-				if len(os.listdir('/media/ubuntu/')) > 0:
-					path_usb_drive = ('/media/ubuntu/' + os.listdir('/media/ubuntu/')[0])
+				# Check if usb storage is attached
+				if os.path.isdir(self.external_path):
+					path_usb_drive = self.external_path
 				else:
-					path_usb_drive = '/home/ubuntu/bagfiles'
+					path_usb_drive = self.internal_path
 
 				# Open sub process for bagrecord
-				self.__proc_rec = subprocess.Popen(['rosbag', 'record', '--split', '--size=1024', '/flagbutton_pressed', '/tf', '/imu/data', '/imu/data_raw', '/imu/mag', '/scan', '/time_reference', '/fix', '/vel', '/cv_camera/image_raw/compressed', '/cv_camera/camera_info', '/rosout'], preexec_fn=os.setsid, cwd=path_usb_drive)
+				command_list = ['rosbag', 'record', '--split', '--size=1024']
+				command_list.extend(self.topic_list)
+				self.__proc_rec = subprocess.Popen(command_list, preexec_fn=os.setsid, cwd=path_usb_drive)
 				# Save log start time
 				self.__rec_time_start = rospy.Time.now()
 				# Return save log start time
-				rec_time = self.__rec_time_start			
+				rec_time = self.__rec_time_start
 				# Check if process call was successful
 				if self.__proc_rec.poll() == None:
 					# Change state
 					self.__state_id = self.__STATE_ID_REC
-					msg_id = 0		
+					msg_id = 0
 				else:
 					rospy.logerr(rospy.get_caller_id() + ': Recording subprocess not started')
 					msg_id = -1
@@ -123,7 +124,7 @@ class Datalogger:
 				rospy.logwarn(rospy.get_caller_id() + ': Invalid request, not recording')
 				msg_id = -1
 				rec_time = rospy.Time(secs = 0, nsecs = 0)
-			
+
 
 		elif request.request_id == self.__REQ_ID_SYS_SHTDN:
 			# Change state
@@ -145,13 +146,17 @@ class Datalogger:
 	def main(self):
 		# Call UserCmd service
 		service_UserCmd = rospy.Service('user_cmd', UserCmd, self.__handle_user_cmd)
-		
+
 		# Keep ROS busy
 		rospy.spin()
 
-      
-if __name__ == '__main__':
-	logger = Datalogger()
-	logger.main()
 
-	
+if __name__ == '__main__':
+	# Read paramters from launch file
+	topic_list = rospy.get_param('remote_bag_logging/topic_list')
+	internal_path = rospy.get_param('remote_bag_logging/int_rec_path')
+	external_path = rospy.get_param('remote_bag_logging/ext_rec_path')
+
+	# Initialize and start class
+	logger = Datalogger(topic_list, internal_path, external_path)
+	logger.main()
--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-echo "Launching datalogger..."
-source /opt/ros/kinetic/setup.bash
-source /home/ubuntu/catkin_ws/devel/setup.bash
-source /etc/ubiquity/env.sh
-roslaunch /home/ubuntu/catkin_ws/src/div_datalogger/launch/div_datalogger.launch
